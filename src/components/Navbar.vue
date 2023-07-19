c56dce3a2375514d9d7b57a98d49ccda047bd940
Guilherme <guilherme.correia@unioeste.br>
2023-07-19 19:51:29 -0300

    Implement Vue Router

---
+++ src/components/Navbar.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="nav">
+    <RouterLink to="/">
+        <img src="" id="logo">
+    </RouterLink>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/pedidos">Pedidos</router-link>
+  </div>
+
+</template>
+
+<script>
+export default{
+    name: "Navbar"
+
+}
+
+</script>
+
+<style scoped>
+
+</style>
